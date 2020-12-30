ea8c0e5b5a7628edf10a4f0c374e9a0b4ff583b9
fernandaandreia <fernanda.nascimento.btk@gmail.com>
2020-12-30 09:20:57 -0300

    configuracao-firebase

---
+++ Agenda/Alunos/Model/AlunoAPI.swift
@@ -0,0 +1,67 @@
+//
+//  AlunoAPI.swift
+//  Agenda
+//
+//  Created by Fernanda Andreia Nascimento on 28/12/20.
+//  Copyright © 2020 Alura. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+
+
+class AlunoAPI: NSObject {
+
+    // MARK: - GET
+    func recuperaAlunos(completion:@escaping() -> Void) {
+        Alamofire.request("http://localhost:8080/api/aluno", method: .get).responseJSON { (response) in
+            switch response.result {
+            case .success:
+                if let resposta = response.result.value as? Dictionary<String, Any> {
+                    guard let listaDeAlunos = resposta["alunos"] as? Array<Dictionary<String, Any>> else { return }
+                    for dicionarioDeAluno in listaDeAlunos {
+                        AlunoDAO().salvaAluno(dicionarioDeAluno: dicionarioDeAluno)
+                    }
+                    completion()
+                }
+                break
+            case .failure:
+                print(response.error!)
+                completion()
+                break
+            }
+        }
+    }
+    
+    
+    // MARK: - PUT
+    func salvaAlunosNoServidor(parametros: Array<Dictionary<String, String>>) {
+        guard let url = URL (string: "http://localhost:8080/api/aluno/lista") else { return }
+        var requisicao = URLRequest(url: url)
+        requisicao.httpMethod = "PUT"
+        
+        let json = try! JSONSerialization.data(withJSONObject: parametros, options: [])
+        
+        requisicao.httpBody = json
+        requisicao.addValue("aplicacao/json", forHTTPHeaderField: "Content-type")
+      
+        Alamofire.request(requisicao)
+    }
+    
+    // MARK: Delete
+    
+    func deletaAluno(id: String){
+        Alamofire.request("http://localhost:8080/api/aluno/\(id)", method: .delete).responseJSON { (resposta) in
+            switch resposta.result {
+            case .failure:
+                
+                print(resposta.result.error!)
+                break
+            default:
+                break
+            }
+        }
+    }
+    
+}
---
+++ Agenda/Alunos/Model/AlunoDAO.swift
@@ -0,0 +1,93 @@
+//
+//  AlunoDAO.swift
+//  Agenda
+//
+//  Created by Fernanda Andreia Nascimento on 28/12/20.
+//  Copyright © 2020 Alura. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+class AlunoDAO: NSObject {
+    
+    var contexto:NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    var gerenciadorDeResultados:NSFetchedResultsController<Aluno>?
+    
+    func recuperaAlunos() -> Array<Aluno> {
+                
+        let pesquisaAluno:NSFetchRequest<Aluno> = Aluno.fetchRequest()
+        let ordenaPorNome = NSSortDescriptor(key: "nome", ascending: true)
+        pesquisaAluno.sortDescriptors = [ordenaPorNome]
+    
+        
+        gerenciadorDeResultados = NSFetchedResultsController(fetchRequest: pesquisaAluno, managedObjectContext: contexto, sectionNameKeyPath: nil, cacheName: nil)
+        
+        
+        do {
+            try gerenciadorDeResultados?.performFetch()
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        guard let listaDeAlunos = gerenciadorDeResultados?.fetchedObjects else { return [] }
+        
+        return listaDeAlunos
+    }
+
+    func salvaAluno(dicionarioDeAluno:Dictionary<String, Any>){
+               
+        var aluno: NSManagedObject?
+        
+        guard let id  = UUID(uuidString: dicionarioDeAluno["id"] as! String) else { return }
+        let alunos = recuperaAlunos().filter(){ $0.id == id }
+       
+        if alunos.count > 0 {
+            guard let alunoEncontrado = alunos.first else { return }
+            aluno = alunoEncontrado
+        }
+        else{
+            let entidade = NSEntityDescription.entity(forEntityName: "aluno", in: contexto)
+            aluno = NSManagedObject(entity: entidade!, insertInto: contexto)
+        }
+
+
+        aluno?.setValue(id, forKey: "id")
+        aluno?.setValue(dicionarioDeAluno["nome"] as? String, forKey: "nome")
+        aluno?.setValue(dicionarioDeAluno["endereco"] as? String, forKey: "endereco")
+        aluno?.setValue(dicionarioDeAluno["telefone"] as? String, forKey: "telefone")
+        aluno?.setValue(dicionarioDeAluno["site"] as? String, forKey: "site")
+        
+        
+        guard let nota = dicionarioDeAluno["nota"] else { return }
+        
+        if (nota is String) {
+            aluno?.setValue((dicionarioDeAluno["nota"] as! NSString).doubleValue, forKey: "nota")
+        }
+        else {
+            let conversaoDeNota = String(describing: nota)
+            aluno?.setValue((dicionarioDeAluno["nota"] as! NSString).doubleValue, forKey: "nota")
+        }
+        
+          atualizaContexto()
+    }
+    
+    func deletaAluno(aluno: Aluno){
+        contexto.delete(aluno)
+        atualizaContexto()
+    }
+    
+    func atualizaContexto(){
+        do {
+            try contexto.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
